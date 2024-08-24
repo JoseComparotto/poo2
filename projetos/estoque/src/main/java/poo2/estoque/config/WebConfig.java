97291fd1afa5910586ad77f3c37de687088db843
José A. Q. C. Gomes <jose.augusto.comparotto@gmail.com>
2024-08-24 13:34:07 -0400

    fix: permitindo todos os metodos no CORS

--- projetos/estoque/src/main/java/poo2/estoque/config/WebConfig.java
+++ projetos/estoque/src/main/java/poo2/estoque/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*");
+            .allowedOrigins("*")
+            .allowedMethods("*");
     }
 }
